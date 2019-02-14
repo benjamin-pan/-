a2b0ea88ba1b022f5db9d9fa1f8160a4927126cc
benjamin <1010629840@qq.com>
2019-02-14 17:00:24 +0800

    bug修复

--- Mobile/js/common.js
+++ Mobile/js/common.js
@@ -19,3 +19,13 @@ function myGet (obj) {
     success: obj.successFn,
   });
 }
+function getAccessToken(){
+  $.ajax({
+    type: "GET",
+    url: "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx39190f186cd2c4ff&secret=01b01c021b008c0d23a7ea0d89976d43",
+    dataType: "json",
+    success: function(data){
+      console.log(data);
+    },
+  });
+}
